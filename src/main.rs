4dbc6e6b6ffff45973b93a296c47f03867ecb45f
sijie-chan <71698137+sijie-chan@users.noreply.github.com>
2024-08-30 22:18:08 +0800

    Update file

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ mod git;
 use git::*;
 
 use rui::*;
+use tokio::time::{interval, Duration};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let repo = open("./")?;//.expect("current repo is not a repo");
-    stage_files(&repo)?;//.expect("stage file error");
-    commit_files(&repo)?;//.expect("commit file failed");
-    push(&repo, "origin")?;
-    Ok(())
+    let mut commit_interval = interval(Duration::from_secs(300));
+
+    loop {
+        commit_interval.tick().await;
+        stage_files(&repo).ok();//.expect("stage file error");
+        commit_files(&repo).ok();//.expect("commit file failed");
+        push(&repo, "origin").ok();
+    }
 }
