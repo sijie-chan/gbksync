f8e6b5cbceddf45fdb35963b0df79f8d2982cd9b
sijie-chan <71698137+sijie-chan@users.noreply.github.com>
2024-08-30 23:46:04 +0800

    Update file

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let repo = open(repo_path)?; //.expect("current repo is not a repo");
     let mut commit_interval = interval(Duration::from_secs(10));
     let mut interval_count = 0;
+    let mut intervals = 0;
 
     loop {
+        info!("Interval {}", intervals);
         commit_interval.tick().await;
         interval_count = (interval_count + 1) % 10;
+        intervals += 1;
         match stage_files(&repo).ok() {
             Some(file_count) if file_count != 0 => {
                 info!("staged {file_count} files");
