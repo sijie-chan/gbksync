44ee1639e6853813e900b32b9851d4b8d9d91372
sijie-chan <71698137+sijie-chan@users.noreply.github.com>
2024-08-30 21:53:02 +0800

    Message

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ use git::*;
 
 use rui::*;
 
-fn main() {
-    let repo = open("./").expect("current repo is not a repo");
-    stage_files(&repo).expect("stage file error");
-    commit_files(&repo).expect("commit file failed");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let repo = open("./")?;//.expect("current repo is not a repo");
+    stage_files(&repo)?;//.expect("stage file error");
+    commit_files(&repo)?;//.expect("commit file failed");
+
+    Ok(())
 }
