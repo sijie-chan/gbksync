9a8b0d9cc79c5293bbfab44a0f88cbdbb7b42c26
sijie-chan <71698137+sijie-chan@users.noreply.github.com>
2024-08-31 08:58:40 +0800

    Update file

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::fs::File;
 use std::rc::Rc;
 use tokio::time::{interval, Duration};
 use tracing::{error, info};
-use tracing_subscriber::{self, fmt, prelude::*};
+use tracing_subscriber::{self, filter::EnvFilter, fmt, prelude::*};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -19,6 +19,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // install global collector configured based on RUST_LOG env var.
     // 设置订阅者
     tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env())
         .with(fmt::layer().with_writer(file))
         .with(fmt::layer().with_writer(std::io::stdout))
         .init();
@@ -48,36 +49,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     rui::rui(app);
 
     return Ok(());
-
-    let repo_path = "/Users/apple/Projects/gbksync";
-    info!("open repository: {}", repo_path);
-    let repo = open(repo_path).expect("current repo is not a repo");
-    let mut commit_interval = interval(Duration::from_secs(10));
-    let mut interval_count = 0;
-    let mut intervals = 0;
-
-    loop {
-        info!("Interval {}", intervals);
-        commit_interval.tick().await;
-        interval_count = (interval_count + 1) % 10;
-        intervals += 1;
-        match stage_files(&repo).ok() {
-            Some(file_count) if file_count != 0 => {
-                info!("staged {file_count} files");
-                //.expect("stage file error");
-                info!("ready to commit files");
-                commit_files(&repo).ok(); //.expect("commit file failed");
-            }
-            _ => {}
-        }
-        if interval_count == 0 {
-            info!("ready to push commits");
-            match push(&repo, "origin") {
-                Ok(_) => {}
-                Err(e) => {
-                    error!("{}", e.to_string());
-                }
-            }
-        }
-    }
 }
